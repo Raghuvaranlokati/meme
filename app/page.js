7f5ffe1dc061588032bddc04d5cf2565420e09d6
raghuvaranlokati <90406864+Raghuvaranlokati@users.noreply.github.com>
2024-07-12 20:33:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ const useCountUp = (end, duration) => {
 
 export default function Home() {
   const followers = useCountUp(57, 2000); // Adjust the numbers as needed
-  const likes = useCountUp(1200, 2000);
+  const likes = useCountUp(1200, 4000);
   const posts = useCountUp(7, 2000);
 
   // Function to handle clicking on a meme (redirecting to Instagram)

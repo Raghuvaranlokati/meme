62e86dbcd1977ec4c9c79301d623dc50b4c9a749
raghuvaranlokati <90406864+Raghuvaranlokati@users.noreply.github.com>
2024-08-08 17:57:14 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -34,7 +34,7 @@ const useCountUp = (end, duration) => {
 };
 
 export default function Home() {
-  const followers = useCountUp(63, 2000); // Adjust the numbers as needed
+  const followers = useCountUp(160, 2000); // Adjust the numbers as needed
   const likes = useCountUp(1200, 4000);
   const posts = useCountUp(10, 2000);
 
